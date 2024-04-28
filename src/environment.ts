4106d21d3c0ba81396c849221e4ac06f3b3a69ad
Marcos Fernando <midianet@gmail.com>
2024-04-27 22:51:27 -0300

    outra tentativa da gambiarra

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const Environment = {
     URL_BASE: import.meta.env.VITE_URL_BASE,
     //172.18.0.50
-    HOME_MENU : '/pages/#/pages/home',
-    QUEMSOMOS_MENU : '/pages/#/pages/quemsomos',
-    EVENTOS_MENU : '/pages/#/pages/eventos'
+    HOME_MENU : 'home',
+    QUEMSOMOS_MENU : 'quemsomos',
+    EVENTOS_MENU : 'eventos'
 
   };
